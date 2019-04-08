2893c64a142eef9dd5c1264c9d92a4ba396e82df
Jessica <676381+shizuka@users.noreply.github.com>
2019-04-07 21:03:31 -0500

    added dark mode

--- js/main.js
+++ js/main.js
@@ -1,2 +1,12 @@
-$(document).ready(function() {
+$(document).ready(function () {
+  
+  $('#toggle-box-checkbox').on("change", function () {
+    if (this.checked) {
+      $('#dark-mode').prop("disabled", false);
+      localStorage.setItem("darkmode", "on");
+    } else {
+      $('#dark-mode').prop("disabled", true);
+      localStorage.setItem("darkmode", "off");
+    }
+  })
 });
