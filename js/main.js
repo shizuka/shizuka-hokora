433cb029d3f258fab043d7bb09e58ce9ea4dcc54
Jessica <676381+shizuka@users.noreply.github.com>
2019-04-11 08:33:53 -0500

    add back to top button, resolve #1

--- js/main.js
+++ js/main.js
@@ -8,5 +8,34 @@ $(document).ready(function () {
       $('#dark-mode').prop("disabled", true);
       localStorage.setItem("darkmode", "off");
     }
-  })
+  });
+
+  var amountScrolled = 300;
+
+  $(".post-listing").scroll(function() {
+    if ( $(".post-listing").scrollTop() > amountScrolled ) {
+      $('a.back-to-top').fadeIn('slow');
+    } else {
+      $('a.back-to-top').fadeOut('slow');
+    }
+  });
+
+  $(window).scroll(function() {
+    if ( $(window).scrollTop() > amountScrolled ) {
+      $('a.back-to-top').fadeIn('slow');
+    } else {
+      $('a.back-to-top').fadeOut('slow');
+    }
+  });
+
+  $('a.back-to-top').click(function () {
+    $('.post-listing').animate({
+      scrollTop: 0
+    }, 700);
+    $('html, body').animate({
+      scrollTop: 0
+    }, 700);
+    return false;
+  });
+
 });
